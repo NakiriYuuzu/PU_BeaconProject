48770ae43cb000b6846ed807bfc44be68fd4d237
NakiriYuuzu <72718339+NakiriYuuzu@users.noreply.github.com>
2021-08-19 20:16:43 +0800

    2021/08/19 - QH 1. Auto Login. 2. Api method.

--- app/src/main/java/tw/edu/pu/pu_smart_campus_micro_positioning_service/Activity/LoginActivity.java
+++ app/src/main/java/tw/edu/pu/pu_smart_campus_micro_positioning_service/Activity/LoginActivity.java
@@ -2,23 +2,40 @@ package tw.edu.pu.pu_smart_campus_micro_positioning_service.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.Settings;
+import android.util.Log;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
 
-import tw.edu.pu.pu_smart_campus_micro_positioning_service.Database.DBHelper;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+import tw.edu.pu.pu_smart_campus_micro_positioning_service.ApiConnect.VolleyApi;
 import tw.edu.pu.pu_smart_campus_micro_positioning_service.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private final String TAG = "Login Debug: ";
+
+    public static String deviceIMEI;
+
+    private String status;
+
     private TextInputEditText etAcc, etPass;
-    private MaterialButton btnLogin, btnRegister;
+    private MaterialButton btnLogin, btnGuest;
+    private CheckBox btnRememberMe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,14 +50,14 @@ public class LoginActivity extends AppCompatActivity {
                 public void run() {
                     finish();
                 }
-            }, 3000);
+            }, 3500);
 
-        }
-        else {
-            findView();
+        } else {
+            viewInit();
             buttonInit();
         }
     }
+
 
     private boolean isNetworkConnected() {
         ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
@@ -51,19 +68,54 @@ public class LoginActivity extends AppCompatActivity {
     private void buttonInit() {
         btnLogin.setOnClickListener(v -> loginFunction());
 
-        btnRegister.setOnClickListener(v -> guestFunction());
+        btnGuest.setOnClickListener(v -> guestFunction());
+
+        SharedPreferences preferences = getSharedPreferences("checkBox", MODE_PRIVATE);
+        String checkBox = preferences.getString("remember", "");
+
+        if (checkBox.equals("true")) {
+            Toast.makeText(getApplicationContext(), "Sign in Successfully!", Toast.LENGTH_SHORT).show();
+            Intent ii = new Intent(getApplicationContext(), Police_MainActivity.class);
+            startActivity(ii);
+
+        } else if (checkBox.equals("false")) {
+            Toast.makeText(getApplicationContext(), "Please Sign In.", Toast.LENGTH_SHORT).show();
+        }
+
+        btnRememberMe.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (buttonView.isChecked()) {
+                    SharedPreferences preferences = getSharedPreferences("checkBox", MODE_PRIVATE);
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putString("remember", "true");
+                    editor.apply();
+
+                    Log.e("checkBox", "checked.");
+
+                } else if (!buttonView.isChecked()) {
+                    SharedPreferences preferences = getSharedPreferences("checkBox", MODE_PRIVATE);
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putString("remember", "false");
+                    editor.apply();
+
+                    Log.e("checkBox", "unchecked.");
+                }
+            }
+        });
     }
 
-    private void findView() {
+    private void viewInit() {
         etAcc = findViewById(R.id.etAcc);
         etPass = findViewById(R.id.etPass);
         btnLogin = findViewById(R.id.btnLogin);
-        btnRegister = findViewById(R.id.btn_Guest);
+        btnGuest = findViewById(R.id.btn_Guest);
+        btnRememberMe = findViewById(R.id.checkBox_rememberMe);
     }
 
+    @SuppressLint("HardwareIds")
     private void loginFunction() {
-        if (etAcc.getText() != null && etPass.getText() != null)
-        {
+        if (etAcc.getText() != null && etPass.getText() != null) {
             String user = etAcc.getText().toString();
             String pass = etPass.getText().toString();
 
@@ -71,28 +123,45 @@ public class LoginActivity extends AppCompatActivity {
                 Toast.makeText(this, "Please enter all the fields!", Toast.LENGTH_SHORT).show();
 
             } else {
+                //getIMEI
+                deviceIMEI = Settings.Secure.getString(this.getContentResolver(), Settings.Secure.ANDROID_ID);
+                Log.e("Yuuzu", deviceIMEI);
 
-                if (user.equals(pass)) {
-                    Toast.makeText(this, "Sign in Successfully!", Toast.LENGTH_SHORT).show();
-                    Intent ii = new Intent(getApplicationContext(), Police_MainActivity.class);
-                    ii.putExtra("ID", user);
-                    startActivity(ii);
-                }
+                VolleyApi volleyApi = new VolleyApi(LoginActivity.this, "https://reqbin.com/echo/post/json");
+
+                volleyApi.post_API_Login(user, pass, deviceIMEI, new VolleyApi.VolleyCallback() {
+                    @Override
+                    public void onSuccess(String result) {
+                        try {
+                            JSONObject requestStatus = new JSONObject(result);
+                            status = requestStatus.getString("success");
+
+                            if (status.equals("true")) {
+                                Toast.makeText(getApplicationContext(), "Sign in Successfully!", Toast.LENGTH_SHORT).show();
+                                Intent ii = new Intent(getApplicationContext(), Police_MainActivity.class);
+                                ii.putExtra("ID", user);
+                                startActivity(ii);
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
             }
 
         } else {
             Toast.makeText(this, "Please enter your account and password!", Toast.LENGTH_SHORT).show();
-            clearFunction();
-        }
-    }
 
-    private void clearFunction() {
-        etAcc.setText("");
-        etPass.setText("");
+        }
     }
 
     private void guestFunction() {
         Intent ii = new Intent(getApplicationContext(), Police_MainActivity.class);
         startActivity(ii);
+    }
+
+    private void loginApiData(VolleyApi.VolleyCallback volleyCallback) {
+
     }
 }
--- app/src/main/java/tw/edu/pu/pu_smart_campus_micro_positioning_service/Activity/Police_MainActivity.java
+++ app/src/main/java/tw/edu/pu/pu_smart_campus_micro_positioning_service/Activity/Police_MainActivity.java
@@ -7,6 +7,7 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.os.Build;
@@ -65,7 +66,13 @@ public class Police_MainActivity extends AppCompatActivity {
             startActivity(ii);
         });
 
-        btnSignOut.setOnClickListener(v -> finish());
+        btnSignOut.setOnClickListener(v -> {
+            SharedPreferences preferences = getSharedPreferences("checkBox", MODE_PRIVATE);
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putString("remember", "false");
+            editor.apply();
+            finish();
+        });
     }
 
     /**
