b278634482e87ad170d78ec3c3c80f33938e8f9a
NakiriYuuzu <72718339+NakiriYuuzu@users.noreply.github.com>
2021-08-19 20:17:13 +0800

    2021/08/19 - QH 1. auto login to another class.

---
+++ app/src/main/java/tw/edu/pu/pu_smart_campus_micro_positioning_service/VariableAndFunction/LoginConstants.java
@@ -0,0 +1,6 @@
+package tw.edu.pu.pu_smart_campus_micro_positioning_service.VariableAndFunction;
+
+public class LoginConstants {
+    public static String KEY_ID = "id";
+    public static String KEY_PASSWORD = "password";
+}
